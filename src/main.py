517c9e2c0a82ea3650b762b706f5f081d16e29a4
Pechalka228 <54882210+Pechalka228@users.noreply.github.com>
2020-12-16 15:10:35 +0300

    Get rid of scattering and noise

--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 
 
-BOARD_ID = BoardIds.SYNTHETIC_BOARD # BRAINBIT_BOARD
-EEG_CHANNELS = BoardShim.get_eeg_channels(BOARD_ID)
+BOARD_ID = 10 # BoardIds.SYNTHETIC_BOARD # BRAINBIT_BOARD
+CHANNEL = 1
 
 
 fig, ax = plt.subplots(1, 1)
@@ -29,6 +29,11 @@ board = BoardShim(BOARD_ID, params)
 board.prepare_session()
 board.start_stream()
 
+points = []
+act_points = []
+
+action = 0
+
 while True:
     time.sleep(0.05)
 
@@ -36,14 +41,28 @@ while True:
     ax.set_xlim(0, 50)
 
     data = board.get_current_board_data(50)
+    if len(data[CHANNEL]) == 0:
+        time.sleep(0.1)
+        continue
 
-    limit = 0
-    for chan in EEG_CHANNELS:
-        limit = max(limit, 2 ** math.ceil(math.log2(max(map(abs, data[chan])))))
+    points.append(2 ** min(11, math.floor(math.log2(max(map(abs, data[CHANNEL]))))))
+    if len(points) > 50:
+        points.pop(0)
 
-    ax.set_ylim(-limit, limit)
-    for chan in EEG_CHANNELS:
-        ax.plot(data[chan])
+    if len(points) > 5:
+        i = 2
+        while i < 4 and i < math.log2(max(points[-i:])) ** 2 / 15:
+            i += 1
+        action = max(points[-i:]) / 1024
+    if action < 256:
+        action = 0
+    act_points.append(action * 1500)
+    if len(act_points) > 50:
+        act_points.pop(0)
+
+    ax.set_ylim(0, 2000)
+    ax.plot(points)
+    ax.plot(act_points)
     plt.draw()
     plt.pause(0.01)
 
