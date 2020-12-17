03b2436bdad03827442c72df0784163a37fba8a7
Ivanq <imachug@yandex.ru>
2020-12-17 18:15:22 +0300

    fire

--- datautil/mneutil.py
+++ datautil/mneutil.py
@@ -12,6 +12,12 @@ from mne.decoding import CSP
 import mne
 from mne.channels import read_layout
 
+from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import ShuffleSplit, cross_val_score
+
+from mne.datasets import eegbci
+
 
 CNT_CHANNELS = 4
 
@@ -44,8 +50,10 @@ ch_types = ["eeg"] * CNT_CHANNELS
 ch_names = BoardShim.get_eeg_names(BoardIds.SYNTHETIC_BOARD)[:CNT_CHANNELS]
 sfreq = BoardShim.get_sampling_rate(BoardIds.SYNTHETIC_BOARD)
 
+
 info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types=ch_types)
 raw = mne.io.RawArray(all_data, info)
+raw.set_montage(mne.channels.make_standard_montage("standard_alphabetic"))
 
 onset = []
 duration = []
@@ -58,18 +66,71 @@ with open(markers_filename) as f:
         description.append(marker["key"])
 
 raw.set_annotations(mne.Annotations(onset=onset, duration=duration, description=description))
+events, _ = mne.events_from_annotations(raw, event_id=lambda s: int(s))
 
-raw.set_montage(mne.channels.make_standard_montage("standard_alphabetic"))
+raw.filter(7., 30., fir_design="firwin")
 
-events_from_annot, event_dict = mne.events_from_annotations(raw, event_id=lambda s: int(s))
-epochs = mne.Epochs(raw, events_from_annot)
+epochs = mne.Epochs(raw, events, proj=True, baseline=None, preload=True)
 epochs.drop_bad()
 
-csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
-csp.fit_transform(epochs.get_data(), epochs.events[:, -1])
-csp.plot_patterns(epochs.info, ch_type="eeg", units="Patterns (AU)", size=1.5).show()
+labels = epochs.events[:, -1]
 
+epochs_data = epochs.get_data()
+cv = ShuffleSplit(10, test_size=0.2, random_state=42)
+cv_split = cv.split(epochs_data)
+
+csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
+lda = LinearDiscriminantAnalysis()
+clf = Pipeline([("CSP", csp), ("LDA", lda)])
+scores = cross_val_score(clf, epochs_data, labels, cv=cv, n_jobs=1)
+
+print(labels)
+class_balance = np.mean(labels == labels[0])
+class_balance = max(class_balance, 1. - class_balance)
+print("Classification accuracy: %f / Chance level: %f" % (np.mean(scores), class_balance))
+
+csp.fit_transform(epochs_data, labels)
+csp.plot_patterns(epochs.info, ch_type='eeg', units='Patterns (AU)', size=1.5)
+
+
+
+sfreq = raw.info['sfreq']
+w_length = int(sfreq * 0.5)   # running classifier: window length
+w_step = int(sfreq * 0.1)  # running classifier: window step size
+w_start = np.arange(0, epochs_data.shape[2] - w_length, w_step)
+
+scores_windows = []
+
+for train_idx, test_idx in cv_split:
+    y_train, y_test = labels[train_idx], labels[test_idx]
+
+    X_train = csp.fit_transform(epochs_data[train_idx], y_train)
+    X_test = csp.transform(epochs_data[test_idx])
+
+    # fit classifier
+    lda.fit(X_train, y_train)
+
+    # running classifier: test classifier on sliding window
+    score_this_window = []
+    for n in w_start:
+        X_test = csp.transform(epochs_data[test_idx][:, :, n:(n + w_length)])
+        score_this_window.append(lda.score(X_test, y_test))
+    scores_windows.append(score_this_window)
+
+# Plot scores over time
+w_times = (w_start + w_length / 2.) / sfreq + epochs.tmin
+
+plt.figure()
+plt.plot(w_times, np.mean(scores_windows, 0), label='Score')
+plt.axvline(0, linestyle='--', color='k', label='Onset')
+plt.axhline(0.5, linestyle='-', color='k', label='Chance')
+plt.xlabel('time (s)')
+plt.ylabel('classification accuracy')
+plt.title('Classification score over time')
+plt.legend(loc='lower right')
 plt.show()
+
+
 
 # for event_type in set(description):
 #     evoked = epochs[event_type].average()
